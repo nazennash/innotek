e0a16eea75c76084dc2b2830650f3dcdf9e24632
nazennash <nazennash120@gmail.com>
2025-03-06 15:27:22 +0300

    routes-change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,9 +32,9 @@ export const Header = () => {
 
   const navItems = [
     { name: 'Home', href: '/' },
-    { name: 'About Us', href: 'about' },
-    { name: 'Services', href: 'services' },
-    { name: 'Contact Us', href: 'contact' },
+    { name: 'About Us', href: '/about' },
+    { name: 'Services', href: '/services' },
+    { name: 'Contact Us', href: '/contact' },
   ];
 
   return (
